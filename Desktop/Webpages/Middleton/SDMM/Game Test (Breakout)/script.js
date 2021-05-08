b95f718984f24fb7d8afdfc5402527767820d62a
David Middleton <middleton.davida@gmail.com>
2021-05-08 12:54:45 -0400

    added collision detection

--- Desktop/Webpages/Middleton/SDMM/Game Test (Breakout)/script.js
+++ Desktop/Webpages/Middleton/SDMM/Game Test (Breakout)/script.js
@@ -8,7 +8,7 @@ var ballRadius = 10;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -19,6 +19,12 @@ function draw() {
     drawBall();
     x += dx;
     y += dy;
+    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
 }
 
 setInterval(draw, 10);
